4ff7ff16a2ab487de1743affd408b936778dbdfd
AshwiniChavan1 <chavan1609@gmail.com>
2022-09-15 13:18:42 +0530

    adding some error palindrome and prime

---
+++ palindrome_problem.js
@@ -0,0 +1,13 @@
+let num=1212;
+let reverse=0;
+let str=num+"";
+while(num>0){
+    let remainder=num%10;
+    reverse=reverse*10+remainder;
+    num=num-remainder;
+    num=num/10;
+}if(reverse==str){
+    console.log("Yes");
+}else{
+    console.log("No");
+}
